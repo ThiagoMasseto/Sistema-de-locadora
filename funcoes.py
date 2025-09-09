1e73cbc5f0be35ce2d0b3e88c651e4c571f34267
ThiagoMasseto <thimasseto@outlook.com>
2025-09-09 14:19:07 -0300

    mudança no arquivo funcoes colocando import time e import os

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,11 @@
 from classes import *
+import time
+import os
 locadorasenai = Locadora()
 def cadastrar_clientes():
     nome=input("Nome--> ")
     cpf=int(input("CPF-->"))
     locadorasenai.cadastrarCliente(nome=nome,cpf=cpf)
     print("Cliente Adicionado com Sucesso!")
+    time.sleep(1)
+    os.system("cls")
