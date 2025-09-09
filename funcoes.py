128f3554df60ef7309d675f955a43daa2c2eed05
ThiagoMasseto <thimasseto@outlook.com>
2025-09-09 14:23:24 -0300

    adicionando try na def cadastrar clientes

--- funcoes.py
+++ funcoes.py
@@ -5,13 +5,15 @@ import os
 locadorasenai = Locadora()
 
 def cadastrar_clientes():
+    try:
+        nome=str(input("Nome--> "))
 
-    nome=input("Nome--> ")
+        cpf=int(input("CPF-->"))
 
-    cpf=int(input("CPF-->"))
+        locadorasenai.cadastrarCliente(nome=nome,cpf=cpf)
+        print("Cliente Adicionado com Sucesso!")
 
-    locadorasenai.cadastrarCliente(nome=nome,cpf=cpf)
-    print("Cliente Adicionado com Sucesso!")
-
-    time.sleep(1)
-    os.system("cls")
+        time.sleep(1)
+        os.system("cls")
+    except Exception as e:
+        print(f" Ocorreu um erro inesperado : {e}")
