65571293f4d456e384a1bcbe4767877c87be9970
ThiagoMasseto <thimasseto@outlook.com>
2025-09-09 14:21:32 -0300

    alguns espaços para o codigo de funcoes ficar bom

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,17 @@
 from classes import *
 import time
 import os
+
 locadorasenai = Locadora()
+
 def cadastrar_clientes():
+
     nome=input("Nome--> ")
+
     cpf=int(input("CPF-->"))
+
     locadorasenai.cadastrarCliente(nome=nome,cpf=cpf)
     print("Cliente Adicionado com Sucesso!")
+
     time.sleep(1)
     os.system("cls")
